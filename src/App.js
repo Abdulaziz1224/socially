5bfaeb8ed1efa4479ebf01e2c52b08fca793073b
Abdulaziz <navoiy498@gmail.com>
2021-10-07 22:16:53 +0500

    a

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route,Link } from "react-router-dom";
-import Design01 from "./component/Web design01/Design01/Design01";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+// import Design01 from "./component/Web design01/Design01/Design01";
 import "./app.scss";
 import Basis from "./component/Asosiy qism/Basis/Basis";
-
-import AuthNumber from "./component/Form/AuthNumber";
-import Login from "./component/Form/Login";
-import Register from "./component/Form/Register";
-import Number from "./component/Form/Number";
-
 
 function App() {
   return (
     <div className="app">
 
       <Router>
-        <Switch> 
+        {/* <Switch> */}
         {/* <Basis /> */}
-        <Design01 />
+        {/* <Design01 /> */}
         {/* <BasicPart /> */}
-        </Switch>
-        <Route path="/authNumber" component={AuthNumber} />
-        <Route path="/number" component={Number} />
-        <Route path="/login" component={Login} />
-        <Route path="/Register" component={Register} />
+        {/* </Switch> */}
+        <Form />
       </Router>
     </div>
   );
