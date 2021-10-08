0e55670aba51b272c89825f0f1b5fdf3deb21425
Abdulaziz <navoiy498@gmail.com>
2021-10-08 18:57:13 +0500

    form is ready conflict correction 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 // import Design01 from "./component/Web design01/Design01/Design01";
 // import Basis from "./component/Asosiy qism/Basis/Basis";
-import Kurs from "./component/Kurs/Kurs";
+// import Kurs from "./component/Kurs/Kurs";
+import BlogPart from "./component/Blog/BlogPart";
 import "./app.scss";
-
-import Number from "./component/Form/Number";
-import AuthNumber from "./component/Form/AuthNumber";
-import Login from "./component/Form/Login";
-import Register from "./component/Form/Register";
 
 function App() {
   return (
@@ -17,15 +13,11 @@ function App() {
 
       <Router>
         {/* <Switch> */}
-
         {/* <Design01 /> */}
         {/* <Basis /> */}
-        <Kurs/>
+        {/* <Kurs/> */}
+        <BlogPart/>
         {/* </Switch> */}
-        <Route path="/number"  component={Number}/>
-        <Route path="/authNumber"  component={AuthNumber}/>
-        <Route path="/login"  component={Login}/>
-        <Route path="/register"  component={Register}/>
       </Router>
     </div>
   );
